f430f940d398fca45a0d21b8add37236c4b9ee89
lamiro3 <lamiro3@naver.com>
2022-06-05 20:33:31 +0900

    프기실 chapter10 연습문제 업뎃

---
+++ Cproject/code/chapter10_prac/1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+	int days[12] = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	for (int i = 1; i < 13; i++) {
+		printf("%d월은 %d일까지 있습니다.\n", i, days[i - 1]);
+	}
+	return 0;
+}
